a26d602e662c00145bb95d5fe13161139c69e5fb
Zish1805 <72144072+Z-8Bit@users.noreply.github.com>
2021-11-24 10:10:02 +0530

    Update qtile-base-setup.sh

--- qtile-base-setup.sh
+++ qtile-base-setup.sh
@@ -3,7 +3,7 @@ git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
 
-paru -S --noconfirm alacritty backlight_control cmatrix dunst figlet fluent-cursor-theme-git i3lock-color lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings lolcat lxappearance maim neofetch nerd-fonts-complete network-manager-applet nitrogen noto-fonts-emoji picom-jonaburg-git pulseaudio pulseaudio-alsa pulseaudio-bluetooth python-dbus python-gobject qogir-gtk-theme-git qtile rofi tela-icon-theme thunar touchegg touche ttf-ms-fonts xclip xorg
+paru -S --noconfirm alacritty backlight_control cmatrix dunst figlet fluent-cursor-theme-git i3lock-color lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings lolcat lxappearance maim neofetch nerd-fonts-complete network-manager-applet nitrogen noto-fonts-emoji picom-jonaburg-git polkit-gnome pulseaudio pulseaudio-alsa pulseaudio-bluetooth python-dbus python-gobject qogir-gtk-theme-git qtile rofi tela-icon-theme thunar touchegg touche ttf-ms-fonts xclip xorg
 
 for service in bluetooth lightdm NetworkManager touchegg.service; do
   sudo systemctl enable --now $service
