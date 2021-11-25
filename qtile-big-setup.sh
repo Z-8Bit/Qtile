eac9cec4d61e91fa3f56b05e07f7ae7176868f7a
Zish1805 <72144072+Z-8Bit@users.noreply.github.com>
2021-11-25 10:23:44 +0530

    Update qtile-big-setup.sh

--- qtile-big-setup.sh
+++ qtile-big-setup.sh
@@ -11,12 +11,13 @@ done
 
 git clone https://github.com/Z-8Bit/Qtile.git ~/Downloads/Qtile
 
-mkdir -p ~/.config/{alacritty,cava,dunst,qtile,rofi,neofetch}
+mkdir -p ~/.config/{alacritty,cava,dunst,qtile,rofi,neofetch,touchegg}
 
 cp ~/Downloads/Qtile/qtile/config.py ~/.config/qtile/
 cp ~/Downloads/Qtile/alacritty/alacritty.yml ~/.config/alacritty/
 cp ~/Downloads/Qtile/dunst/dunstrc ~/.config/dunst/
 cp ~/Downloads/Qtile/rofi/* ~/.config/rofi/
+cp ~/Downloads/Qtile/touchegg/touchegg.conf ~/.config/touchegg/
 cp ~/Downloads/Qtile/neofetch/config.conf ~/.config/neofetch/
 cp ~/Downloads/Qtile/picom.conf ~/.config/
 
